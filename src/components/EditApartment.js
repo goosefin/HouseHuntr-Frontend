a33d5c6f36b76c0d26bf8247270cd551c316be67
goosefin <agustinafin96@gmail.com>
2022-11-22 19:44:40 -0500

    fixed file structure for deployment

--- src/components/EditApartment.js
+++ src/components/EditApartment.js
@@ -39,6 +39,21 @@ class EditApartment extends Component{
         this.props.toggleForm()
     }
 
+    toggleCheck = (e) =>{
+        // console.log(e)
+        if(e.target.value === 'on'){
+            console.log('inside if', e.target.checked)
+            this.setState({
+                [e.target.checked]:false
+            })
+        }else{
+            console.log('inside else', e.target.checked)
+            this.setState({
+                [e.target.checked]:true
+            })
+        }
+    }
+
     render(){
         return(
             <div>
@@ -51,25 +66,25 @@ class EditApartment extends Component{
                     <label htmlFor="price">Price:</label>
                     <input type="number" id="price" name="price" onChange={this.handleChange} value={this.state.price}/>
                     <label htmlFor="cats">Cats:</label>
-                    <input type="checkbox" id="cats" name="address" onChange={this.handleChange} value={this.state.cats} checked={this.state.cats}/>
+                    <input type="checkbox" id="cats" name="cats" onChange={this.handleChange} checked={this.state.cats} onClick={this.toggleCheck}/>
                     <label htmlFor="dogs">Dogs:</label>
-                    <input type="checkbox" id="dogs" name="dogs" onChange={this.handleChange} value={this.state.dogs} checked={this.state.dogs}/>
+                    <input type="checkbox" id="dogs" name="dogs" onChange={this.handleChange} checked={this.state.dogs}/>
                     <label htmlFor="washer">Washer:</label>
-                    <input type="checkbox" id="washer" name="washer" onChange={this.handleChange} value={this.state.washer} checked={this.state.washer}/>
+                    <input type="checkbox" id="washer" name="washer" onChange={this.handleChange} checked={this.state.washer}/>
                     <label htmlFor="dryer">Dryer:</label>
-                    <input type="checkbox" id="dryer" name="dryer" onChange={this.handleChange} value={this.state.dryer} checked={this.state.dryer}/>
+                    <input type="checkbox" id="dryer" name="dryer" onChange={this.handleChange} checked={this.state.dryer}/>
                     <label htmlFor="dishwasher">Dishwasher:</label>
-                    <input type="checkbox" id="dishwasher" name="dishwasher" onChange={this.handleChange} value={this.state.dishwasher} checked={this.state.dishwasher}/>
+                    <input type="checkbox" id="dishwasher" name="dishwasher" onChange={this.handleChange} checked={this.state.dishwasher}/>
                     <label htmlFor="outdoor_space">Outdoor Space:</label>
-                    <input type="checkbox" id="outdoor_space" name="outdoor_space" onChange={this.handleChange} value={this.state.outdoor_space} checked={this.state.outdoor_space}/>
+                    <input type="checkbox" id="outdoor_space" name="outdoor_space" onChange={this.handleChange} checked={this.state.outdoor_space}/>
                     <label htmlFor="elevator">Elevator:</label>
-                    <input type="checkbox" id="elevator" name="elevator" onChange={this.handleChange} value={this.state.elevator} checked={this.state.elevator}/>
+                    <input type="checkbox" id="elevator" name="elevator" onChange={this.handleChange} checked={this.state.elevator}/>
                     <label htmlFor="doorman">Doorman:</label>
-                    <input type="checkbox" id="doorman" name="doorman" onChange={this.handleChange} value={this.state.doorman} checked={this.state.dogs}/>
+                    <input type="checkbox" id="doorman" name="doorman" onChange={this.handleChange} checked={this.state.dogs}/>
                     <label htmlFor="link">Link:</label>
                     <input type="text" id="link" name="link" onChange={this.handleChange} value={this.state.link}/>
                     <label htmlFor="scheduled_showing">Scheduled Showing:</label>
-                    <input type="checkbox" id="scheduled_showing" name="scheduled_showing" onChange={this.handleChange} value={this.state.scheduled_showing} checked={this.state.scheduled_showing}/>
+                    <input type="checkbox" id="scheduled_showing" name="scheduled_showing" onChange={this.handleChange} checked={this.state.scheduled_showing}/>
                     <label htmlFor="scheduled_showing_time">Scheduled Showing Time:</label>
                     <input type="text" id="scheduled_showing_time" name="scheduled_showing_time" onChange={this.handleChange} value={this.state.scheduled_showing_time}/>
                     <label htmlFor="seen">Seen:</label>
