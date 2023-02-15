f5a5085b7bc0bf2922e28609fef373b1bdca78a5
goosefin <agustinafin96@gmail.com>
2023-02-14 19:06:27 -0500

    styled new nav bar on landing page

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import Register from './Register'
 import Login from './Login'
+import {Form, Button, FloatingLabel} from 'react-bootstrap'
 
 class Landing extends Component{
     constructor(props){
@@ -14,25 +15,26 @@ class Landing extends Component{
     render(){
         return(
             <div id="landing">
-                <div id="title-des">
-                    <div id="title">
-                        <img src="./fullLogo.png" id="landing-logo" alt="yellow circle with black house"/>
-                        <h1>HouseHuntr</h1> 
-                    </div>
-
+                <div id="title">
+                    <img src="./Logo.png" id="landing-logo" alt="yellow circle with black house"/>
+                    <h1>HouseHuntr</h1> 
+                    <Button id="landing-reg-log" variant="outline-secondary">Register/Log In</Button>
+                </div>
+                
+                {/* <div id="title-des">
                     <div id="description">
                         <h3>Organize your apartment search</h3>
                         <p>Moving is considered to be one of the top 3 most stressfull events in a persons life. HouseHuntr simplifies the search by optimizing the ability to track the apartment you like from any platform you use. Our platform was designed with simplicity in mind, just upload all of the information related to the listing and you use our dashboard to update as you progress through the application process</p>
                         <h6><b>Create an account now for free or log in to start!</b></h6>
                     </div>
-                </div>
+                </div> */}
 
-                <div id="log-reg">
+                {/* <div id="log-reg">
                     <h3>Register</h3>
                     <Register register={this.props.register} handleChange={this.props.handleChange}/>
                     <h3>Log In</h3>
                     <Login login={this.props.login} handleChange={this.props.handleChange}/>
-                </div>
+                </div> */}
             </div>
         )
     }
